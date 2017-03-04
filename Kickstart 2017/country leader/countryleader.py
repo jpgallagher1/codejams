2e5e46958ea645e8638b2a871d8a80bf90dfa99d
Dylan Rodriquez <dylanmr@uchicago.edu>
2017-03-03 18:45:10 -0600

    Parsing update

---
+++ Kickstart 2017/country leader/countryleader.py
@@ -0,0 +1,65 @@
+'''
+Problem
+
+The Constitution of a certain country states that the leader is the
+person with the name containing the greatest number of different
+alphabet letters.
+(The country uses the uppercase English alphabet from A through Z.)
+For example, the name GOOGLE has four different alphabet letters:
+E, G, L, and O.
+The name APAC CODE JAM has eight different letters.
+If the country only consists of these 2 persons,
+APAC CODE JAM would be the leader.
+
+If there is a tie, the person whose name comes earliest
+in alphabetical order is the leader.
+
+Given a list of names of the citizens of the country,
+can you determine who the leader is?
+
+Input
+
+The first line of the input gives the number of test cases, T.
+T test cases follow.
+Each test case starts with a line with an interger N,
+the number of people in the country.
+Then N lines follow.
+The i-th line represents the name of the i-th person.
+Each name contains at most 20 characters
+and contains at least one alphabet letter.
+
+Output
+
+For each test case, output one line containing
+Case #x: y,
+where x is the test case number (starting from 1)
+and y is the name of the leader.
+
+Small dataset
+
+Each name consists of at most 20 characters and only consists of the
+uppercase English letters A through Z.
+
+Large dataset
+
+Each name consists of at most 20 characters and only consists of the
+uppercase English letters A through Z and ' '(space).
+All names start and end with alphabet letters.
+'''
+import numpy as np
+fname = "A-small-practice.in.txt"
+
+
+def importData(filePath):
+    textArray = np.loadtxt(fname, dtype="string")
+    result = []
+    addToResult = result.append
+    for item in textArray[1:]:
+        if item.isalpha() is not True:
+            addToResult(textArray[list(textArray).index(item) + 1:
+                                  list(textArray).index(item) + int(item) + 1])
+        else:
+            pass
+    return result
+
+print importData(fname)
