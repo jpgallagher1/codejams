88eae6f550ebd39f5be5ae6d2d2e4e17721f0f4d
Dylan Rodriquez <dylanmr@uchicago.edu>
2017-03-08 17:39:02 -0600

    Need to check final. multiplied minimum numbers of both vectors

--- 2008 1A/minimumscalarproduct.py
+++ 2008 1A/minimumscalarproduct.py
@@ -1,8 +1,34 @@
 fname = "A-small-practice.in.txt"
 
 def importData(filePath):
-	caseCount = 0
 	for line in open(filePath):
-		print map(int, (line.strip()).split())
+		yield map(int, (line.strip()).split())
 
-importData(fname)
+def multiply(a, b):
+	return a*b
+
+def calculate(listOfVectors):
+	caseCount = listOfVectors[0]
+	vectorCount = 0
+	vectors = []
+	i = 0
+	while i < caseCount[0]:
+		vectorDescriptorCheck = listOfVectors[i+1]
+		if len(vectorDescriptorCheck) == 1:
+			vectorCount = vectorDescriptorCheck
+			v1 = listOfVectors[i+2]
+			v2 = listOfVectors[i+3]
+			v1.sort()
+			v2.sort()
+			v2.reverse()
+			print [v1*v2 for v1,v2 in zip(v1,v2)]
+		else:
+			pass
+		i += 3
+	return vectors
+
+
+
+
+
+calculate([x for x in importData(fname)])
