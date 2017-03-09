80b3c99c40c83dc10ca43b6a28bb481815ffbc7d
Dylan Rodriquez <dylanmr@uchicago.edu>
2017-03-09 11:07:46 -0600

    Organizes cases and sets of vectors

--- 2008 1A/minimumscalarproduct.py
+++ 2008 1A/minimumscalarproduct.py
@@ -1,23 +1,9 @@
 
-#def calculate(listOfVectors):
-#	caseCount = listOfVectors[0]
-#	vectorCount = 0
-#	vectors = []
-#	i = 0
-#	while i < caseCount[0]:
-#		vectorDescriptorCheck = listOfVectors[i+1]
-#		if len(vectorDescriptorCheck) == 1:
-#			vectorCount = vectorDescriptorCheck
-#			v1 = listOfVectors[i+2]
-#			v2 = listOfVectors[i+3]
-#			v1.sort()
-#			v2.sort()
-#			v2.reverse()
-#			print [v1*v2 for v1,v2 in zip(v1,v2)]
-#		else:
-#			pass
-#		i += 3
-#	return vectors
+#def storeVectors(vectorCount, caseNumber):
+#	count = vectorCount
+#	for caseCount in xrange(2, caseNumber+2):
+#		n =
+
 
 def sort(list):
 	l = list
@@ -36,8 +22,12 @@ def minimumProduct(v1, v2):
 
 if __name__ == "__main__" :
 	case = int(raw_input())
-	
 	for caseCount in xrange(1, case+1):
-		n = [int(s) for s in raw_input().split(" ")]
-		
-		print ("Case #%i: %s" % (caseCount, sort(n)))
+		i = 0
+		vecSet = []
+		addToVec = vecSet.append 
+		while i < 3:
+			n = [int(s) for s in raw_input().split(" ")]
+			addToVec(n)
+			i += 1
+		print ("Case #%i: %s" % (caseCount, vecSet))
