5c033de33efe15f43bc178df1d17c569016ee192
Dylan Rodriquez <dylanmr@uchicago.edu>
2017-03-09 10:01:01 -0600

    Using raw input for data stream

--- 2008 1A/minimumscalarproduct.py
+++ 2008 1A/minimumscalarproduct.py
@@ -1,31 +1,31 @@
-fname = "A-small-practice.in.txt"
-
-def importData(filePath):
-	for line in open(filePath):
-		yield map(int, (line.strip()).split())
-
-def calculate(listOfVectors):
-	caseCount = listOfVectors[0]
-	vectorCount = 0
-	vectors = []
-	i = 0
-	while i < caseCount[0]:
-		vectorDescriptorCheck = listOfVectors[i+1]
-		if len(vectorDescriptorCheck) == 1:
-			vectorCount = vectorDescriptorCheck
-			v1 = listOfVectors[i+2]
-			v2 = listOfVectors[i+3]
-			v1.sort()
-			v2.sort()
-			v2.reverse()
-			print [v1*v2 for v1,v2 in zip(v1,v2)]
-		else:
-			pass
-		i += 3
-	return vectors
-
-
+if name == "__main__" :
+	case = raw_input()
+	
+	for caseCount in xrange(1, case+1):
+		testCase = raw_input()
+		print ("Case #%i: %s" % (caseNr, testCase))
+#def calculate(listOfVectors):
+#	caseCount = listOfVectors[0]
+#	vectorCount = 0
+#	vectors = []
+#	i = 0
+#	while i < caseCount[0]:
+#		vectorDescriptorCheck = listOfVectors[i+1]
+#		if len(vectorDescriptorCheck) == 1:
+#			vectorCount = vectorDescriptorCheck
+#			v1 = listOfVectors[i+2]
+#			v2 = listOfVectors[i+3]
+#			v1.sort()
+#			v2.sort()
+#			v2.reverse()
+#			print [v1*v2 for v1,v2 in zip(v1,v2)]
+#		else:
+#			pass
+#		i += 3
+#	return vectors
 
 
 
-calculate([x for x in importData(fname)])
+
+
+#calculate([x for x in importData(fname)])
